1c132651a0a36b3398296acc5cceb5670567fd83
hyemin <whgpals641@naver.com>
2024-10-21 16:59:17 +0900

    241021

--- Final project/src/components/Footer.vue
+++ Final project/src/components/Footer.vue
@@ -1,7 +1,41 @@
 <template>
-  <div>푸터</div>
+  <section id="footer">
+    <div id="footer_text">
+      <ul>
+        <li>회사소개</li>
+        <li>개인정보처리방침</li>
+        <li>이용약관</li>
+      </ul>
+      <p>
+        (주)CHERRISH | 대표이사 : 태영 김 | 사업자등록번호 : 123-45-67891 |
+        통신판매업 : 제 2024-대구반월D-0505호 대구광역시 달서구 반월로 67
+        그린컴퓨터아트학원 5층 | 호스팅 서비스 : 네이버 클라우드 (주)
+      </p>
+      <p>
+        신한은행 채무지급보증 안내 당사는 고객님의 현금 결제 금액에 대해
+        신한은행과 채무지급보증 계약을 체결하여 안전거래를 보장하고
+        있습니다.서비스가입 사실 확인
+      </p>
+      <p>
+        (주)CHERRISH는 통신판매 중개자로서 통신판매의 당사자가 아닙니다. 본
+        상품은 개별판매자가 등록한 상품으로 상품, 상품정보, 거래에 관한 의무와
+        책임은 각 판매자에게 있습니다. 단, 이용약관 및 정책, 기타 거래 체결
+        과정에서 고지하는 내용 등에 따라 검수하고 보증하는 내용에 대한 책임은
+        (주)CHERRISH에 있습니다.
+      </p>
+    </div>
+  </section>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+#footer {
+  background-color: var(--color-main-Lgray);
+  margin: 0 auto;
+}
+#footer_text{
+  max-width: var(--main-max-width);
+  width: 100vw;
+}
+</style>
